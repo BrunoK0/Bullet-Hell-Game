47cbed0eaa8bee23eebc3dd29f1b02086ec686a6
BrunoK0 <bruno.koga0@gmail.com>
2021-11-01 15:24:05 -0300

    An attempt at creating a more versatile generator
    Trying to create a better generator

--- bullet generator/generator.gd
+++ bullet generator/generator.gd
@@ -1,38 +1,67 @@
 extends Node2D
 
+enum SPAWNER_MODES {LINEAR, AIMED, FIXED}
+enum BULLET_MODES {FIXED}
+
+export var spawner_mode = SPAWNER_MODES.FIXED
+var current_spawner_mode = SPAWNER_MODES.FIXED
+
 var bullet_scene = preload("res://bullets/basic/basicBullet.tscn")
 onready var shoot_timer = $ShootTimer
-onready var rotater = $Rotator
+onready var spawn_points = $SpawnPoints
 
 export var rotation_speed = 0
 export var shoot_timer_wait_time = 0.2
-export var spawn_point_number = 4
-export var arch_angle = 180
+export (int) var spawn_point_number = 4
 export var radius = 1
-export (bool) var follow_player = false
 
-func _ready():	
-	var step = deg2rad(arch_angle) / spawn_point_number
-	
-	for i in range(spawn_point_number + 1):
-		var spawn_point = Node2D.new()
-		var pos = Vector2(radius, 0).rotated(step * i)
-		spawn_point.position = pos
-		spawn_point.rotation = pos.angle()
-		rotater.add_child(spawn_point)
-		
+export var arc_angle = 90
+
+func _ready():
+	create_spawners()
 	shoot_timer.wait_time = shoot_timer_wait_time
 	shoot_timer.start()
 
 
 func _process(delta):
-	var new_rotation = rotater.rotation_degrees + rotation_speed * delta
-	rotater.rotation_degrees = fmod(new_rotation, 360)
+	var new_rotation = rotation_degrees + rotation_speed * delta
+	rotation_degrees = fmod(new_rotation, 360)
+	if(spawner_mode != current_spawner_mode):
+		create_spawners()
+
+func create_spawners():
+	if(spawn_point_number < 1):
+		spawn_point_number = 1
+		
+	if(radius <= 0):
+		radius = 0.001
+	
+	for i in range(0, spawn_point_number):
+		var spawn_point = Node2D.new()
+		var step = float(i) / float(spawn_point_number)
+		var pos = Vector2(radius, 0).rotated(arc_angle * step)
+		spawn_point.position = pos
+		spawn_point.rotation_degrees = pos.angle()
+		print_debug(spawn_point)
+		spawn_points.add_child(spawn_point)
+		
+		
+
+func fixed_angle_spawner():
+	for i in range(0, spawn_point_number):
+		var spawn_point = Node2D.new()
+		var step = float(i) / float(spawn_point_number)
+		
+
+
+func shoot_bullets():
+	for j in spawn_points.get_children():
+		var bullet = bullet_scene.instance()
+		get_tree().root.add_child(bullet)
+		bullet.position = j.global_position
+		bullet.rotation = j.global_rotation
 	
 
 func _on_ShootTimer_timeout() -> void:
-	for s in rotater.get_children():
-		var bullet = bullet_scene.instance()
-		get_tree().root.add_child(bullet)
-		bullet.position = s.global_position
-		bullet.rotation = s.global_rotation	
+	#print_debug("shoot bullets")
+	shoot_bullets()
--- bullets/basic/basicBullet.gd
+++ bullets/basic/basicBullet.gd
@@ -1,7 +1,16 @@
 extends Node2D
 
-export var speed = 100
+export (float) var speed = 200;
+export (float) var lifetime = 10.00;
+export (Vector2) var velocity = Vector2();
+export (bool) var use_velocity; # If true use velocity, if false use rotation
+export (float) var rotation_change;
 
 
 func _process(delta):
-	position += transform.x * speed * delta
+	if use_velocity:
+		position += velocity.normalized() * speed * delta;
+	else:
+		position += Vector2(cos(rotation), -sin(rotation)) * speed * delta;
+	
+	rotation_degrees += rotation_change * delta;
